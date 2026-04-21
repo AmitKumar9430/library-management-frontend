8e41232b5a1c0f35579ecc90ecf57a2c99a9c59e
AmitKumar9430 <maapapa9430@gmail.com>
2026-04-21 14:28:52 +0530

    Updated frontend UI and fixes

--- src/api/axios.js
+++ src/api/axios.js
@@ -1,8 +1,8 @@
 import axios from 'axios'
 
 const api = axios.create({
-//  baseURL: 'https://library-management-backend-1-8tr2.onrender.com/api',
-  baseURL: 'http://localhost:8080/api',
+  baseURL: 'https://library-management-backend-1-8tr2.onrender.com/api',
+//  baseURL: 'http://localhost:8080/api',
   headers: { 'Content-Type': 'application/json' }
 })
 
